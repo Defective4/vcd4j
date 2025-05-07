e2947ea47de50cd98a480b41b01a0c37ab9fe1c8
Defective <def3ctive4@gmail.com>
2025-05-08 00:15:38 +0200

    Check value's bit count in multi-bit change entry

--- src/main/java/io/github/defective4/dsp/vcd4j/data/MultibitChangeEntry.java
+++ src/main/java/io/github/defective4/dsp/vcd4j/data/MultibitChangeEntry.java
@@ -7,6 +7,9 @@ public final class MultibitChangeEntry extends ChangeEntry<Integer> {
     public MultibitChangeEntry(VariableDefinition variable, int value) {
         super(variable, value);
         if (value < -1) throw new IllegalArgumentException("value < -1");
+        if (!isUndefined() && Integer.toBinaryString(value).length() > variable.getBitCount())
+            throw new IllegalArgumentException(
+                    "value must take less or equal bits to the defined variable's bitCount");
     }
 
     @Override
