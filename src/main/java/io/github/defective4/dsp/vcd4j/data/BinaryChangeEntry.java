bf8bb2e7300386d28b6418c0e00b90e610d52598
Defective <def3ctive4@gmail.com>
2025-05-08 00:11:43 +0200

    Additional check in binary change entry constructor

--- src/main/java/io/github/defective4/dsp/vcd4j/data/BinaryChangeEntry.java
+++ src/main/java/io/github/defective4/dsp/vcd4j/data/BinaryChangeEntry.java
@@ -3,6 +3,8 @@ package io.github.defective4.dsp.vcd4j.data;
 public final class BinaryChangeEntry extends ChangeEntry<State> {
     public BinaryChangeEntry(VariableDefinition variable, State value) {
         super(variable, value);
+        if (variable.getBitCount() != 1)
+            throw new IllegalArgumentException("Only 1-bit variables are allowed in binary change entry");
     }
 
     @Override
